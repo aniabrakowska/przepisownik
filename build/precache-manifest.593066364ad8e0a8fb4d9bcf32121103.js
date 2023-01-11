5fb6923068d4702d804719e727dbc625974b3e8e
Ania <brakowska.anna@gmail.com>
2023-01-11 19:52:41 +0100

    configuration GitHub Pages for React

---
+++ build/precache-manifest.593066364ad8e0a8fb4d9bcf32121103.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d09a98f7f70888c9e9e",
+    "url": "/przepisownik/static/css/main.9beefd24.chunk.css"
+  },
+  {
+    "revision": "0d09a98f7f70888c9e9e",
+    "url": "/przepisownik/static/js/main.0d09a98f.chunk.js"
+  },
+  {
+    "revision": "b6c1f68513ee20ff8347",
+    "url": "/przepisownik/static/css/1.a9c00942.chunk.css"
+  },
+  {
+    "revision": "b6c1f68513ee20ff8347",
+    "url": "/przepisownik/static/js/1.b6c1f685.chunk.js"
+  },
+  {
+    "revision": "5424f3efc94ed22712a2",
+    "url": "/przepisownik/static/js/runtime~main.5424f3ef.js"
+  },
+  {
+    "revision": "0b9146d177f0041e37b29f2ed210c57c",
+    "url": "/przepisownik/static/media/default.0b9146d1.jpg"
+  },
+  {
+    "revision": "7d52c4aec4e6b8074bdf6376e75e6ff4",
+    "url": "/przepisownik/static/media/fish.7d52c4ae.jpg"
+  },
+  {
+    "revision": "9f77d1cb71ea985d8abc6baf26c330c3",
+    "url": "/przepisownik/static/media/breakfast.9f77d1cb.jpg"
+  },
+  {
+    "revision": "e0e5bb255a6fd86a86f154e952f573e5",
+    "url": "/przepisownik/static/media/pasta.e0e5bb25.jpg"
+  },
+  {
+    "revision": "ae20c9927a2df434f4bb102338ba5ad9",
+    "url": "/przepisownik/static/media/groats.ae20c992.jpg"
+  },
+  {
+    "revision": "6e4070bddceda9a3200e3209a1ee0d0a",
+    "url": "/przepisownik/static/media/salad.6e4070bd.jpg"
+  },
+  {
+    "revision": "111f7c0da32c6b6b11d6fdeab1bbd392",
+    "url": "/przepisownik/static/media/dessert.111f7c0d.jpg"
+  },
+  {
+    "revision": "fe64ee411382ec1327fafec92694d99e",
+    "url": "/przepisownik/index.html"
+  }
+];
